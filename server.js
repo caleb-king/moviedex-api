42927204ef1746f2d8c750b042ab5a61d685249b
Caleb King <caleb.king536@gmail.com>
2020-03-26 17:12:13 -0600

    add authorization header and validation for requests

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -5,12 +6,25 @@ const helmet = require('helmet');
 const MOVIES = require('./movies-data-small.json');
 
 const app = express();
-app.use(morgan('dev'));
-app.use(cors());
-app.use(helmet());
 
-app.get('/', (req, res) => {
-  res.send('hi');
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || apiToken !== authToken.split(' ')[1]) {
+    return res
+      .status(401)
+      .json({ error: 'unauthorized request' });
+  }
+  next();
+});
+
+app.get('/movie', (req, res) => {
+  res.json(MOVIES[0]);
 });
 
 const PORT = 8000;
