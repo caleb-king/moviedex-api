2880e9f60be7e5fbff907450fe0b0ed615f6fe43
Caleb King <caleb.king536@gmail.com>
2020-03-26 20:40:36 -0600

    application set up for deploying to Heroku

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data-small.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -61,8 +62,16 @@ app.get('/movie', function handleGetMovie(req, res) {
   res.json(returnedMovies);
 });
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`server running on PORT ${PORT}`);
-});
+app.listen(PORT);
